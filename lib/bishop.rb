ac397006374345e92508ddd0006e3f1969546084
brandonricharda <brandonricharda@gmail.com>
2020-08-23 21:09:59 -0400

    Refactored Bishop so it uses new all_directions module.

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -1,4 +1,6 @@
+require_relative "all_directions.rb"
 require_relative "player.rb"
+include All_Directions
 
 class Bishop < Player
     attr_accessor :symbol
@@ -9,23 +11,9 @@ class Bishop < Player
     end
 
     def potential_moves
-        result = []
         x = @position[0]
         y = @position[1]
-
-        until x == 0 || y == 0
-            x -= 1
-            y -= 1
-        end
-
-        until x > 7 || y > 7
-            result << [x, y] unless [x, y] == @position
-            x += 1
-            y += 1
-        end
-
-        result
-
+        diagonal(x, y, @position)
     end
 
 end
