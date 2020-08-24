302c59b400813e4af24ba500efbf73c2db57e3bf
brandonricharda <brandonricharda@gmail.com>
2020-08-23 21:15:51 -0400

    Refactored Rook to use new all_directions module.

--- lib/rook.rb
+++ lib/rook.rb
@@ -1,4 +1,6 @@
+require_relative "all_directions.rb"
 require_relative "player.rb"
+include All_Directions
 
 class Rook < Player
     attr_accessor :symbol
@@ -9,22 +11,9 @@ class Rook < Player
     end
 
     def potential_moves
-        result = []
         x = @position[0]
         y = @position[1]
-
-        if @team == "black"
-            #assigns potential moves in a cross formation focused on the current spot
-            0.upto(7).each { |x| result << [x, y] unless [x, y] == @position }
-            0.upto(7).each { |y| result << [x, y] unless [x, y] == @position }
-        elsif @team == "white"
-            #assigns potential moves for the white team, which sits at the other end of the board
-            7.downto(0).each { |x| result << [x, y] unless [x, y] == @position }
-            7.downto(0).each { |y| result << [x, y] unless [x, y] == @position }
-        end
-
-        result
-
+        linear(x, y, @position, @team)
     end
 
 end
