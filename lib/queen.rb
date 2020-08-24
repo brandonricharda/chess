9ff74e730c2761138abf2b56ceb912c878a002bb
brandonricharda <brandonricharda@gmail.com>
2020-08-23 21:14:19 -0400

    Refactored Queen to use new all_directions module.

--- lib/queen.rb
+++ lib/queen.rb
@@ -1,10 +1,18 @@
+require_relative "all_directions.rb"
 require_relative "player.rb"
+include All_Directions
 
 class Queen < Player
     attr_accessor :symbol
-    def initialize(color, starting)
-        super(starting)
+    def initialize(position, color)
+        super(position, color)
         options = {"white" => "\u2655", "black" => "\u265B"}
         @symbol = options[color]
+    end
+
+    def potential_moves
+        x = @position[0]
+        y = @position[1]
+        diagonal(x, y, @position) + linear(x, y, @position, @team)
     end
 end
