88788230e747d308e99f2fc043f89c978da0a11d
brandonricharda <brandonricharda@gmail.com>
2020-08-26 21:37:02 -0400

    Moves directional methods into module.

--- lib/all_directions.rb
+++ lib/all_directions.rb
@@ -2,56 +2,56 @@ require_relative "board.rb"
 
 module All_Directions
 
-end
+    def vertices
+        Board.new.vertices.keys
+    end
 
-#need to incorporate the methods below into the module so everything (including @vertices) is accessible to files that access it
+    def top(arr)
+        vertices.select { |coordinates| coordinates[0] == arr[0] && coordinates[1] > arr[1] }
+    end
 
-@vertices = Board.new.vertices.keys
+    def bottom(arr)
+        vertices.select { |coordinates| coordinates[0] == arr[0] && coordinates[1] < arr[1] }
+    end
 
-def top(arr)
-    @vertices.select { |coordinates| coordinates[0] == arr[0] && coordinates[1] > arr[1] }
-end
+    def left(arr)
+        vertices.select { |coordinates| coordinates[0] < arr[0] && coordinates[1] == arr[1] }
+    end
 
-def bottom(arr)
-    @vertices.select { |coordinates| coordinates[0] == arr[0] && coordinates[1] < arr[1] }
-end
+    def right(arr)
+        vertices.select { |coordinates| coordinates[0] > arr[0] && coordinates[1] == arr[1] }
+    end
 
-def left(arr)
-    @vertices.select { |coordinates| coordinates[0] < arr[0] && coordinates[1] == arr[1] }
-end
+    def top_right(arr, result = [])
+        return result if arr[0] == 7 || arr[1] == 7
+        square_above = top(arr).first
+        diagonal = right(square_above).first
+        result << diagonal
+        top_right(diagonal, result)
+    end
 
-def right(arr)
-    @vertices.select { |coordinates| coordinates[0] > arr[0] && coordinates[1] == arr[1] }
-end
+    def top_left(arr, result = [])
+        return result if arr[0] == 0 || arr[1] == 7
+        square_above = top(arr).first
+        diagonal = left(square_above).last
+        result << diagonal
+        top_left(diagonal, result)
+    end
 
-def top_right(arr, result = [])
-    return result if arr[0] == 7 || arr[1] == 7
-    square_above = top(arr).first
-    diagonal = right(square_above).first
-    result << diagonal
-    top_right(diagonal, result)
-end
+    def bottom_right(arr, result = [])
+        return result if arr[0] == 7 || arr[1] == 0
+        square_below = bottom(arr).last
+        diagonal = right(square_below).first
+        result << diagonal
+        bottom_right(diagonal, result)
+    end
 
-def top_left(arr, result = [])
-    return result if arr[0] == 0 || arr[1] == 7
-    square_above = top(arr).first
-    diagonal = left(square_above).last
-    result << diagonal
-    top_left(diagonal, result)
-end
+    def bottom_left(arr, result = [])
+        return result if arr[0] == 0 || arr[1] == 0
+        square_below = bottom(arr).last
+        diagonal = left(square_below).last
+        result << diagonal
+        bottom_left(diagonal, result)
+    end
 
-def bottom_right(arr, result = [])
-    return result if arr[0] == 7 || arr[1] == 0
-    square_below = bottom(arr).last
-    diagonal = right(square_below).first
-    result << diagonal
-    bottom_right(diagonal, result)
-end
-
-def bottom_left(arr, result = [])
-    return result if arr[0] == 0 || arr[1] == 0
-    square_below = bottom(arr).last
-    diagonal = left(square_below).last
-    result << diagonal
-    bottom_left(diagonal, result)
 end
