22a0a0fad4817001a3fb9d3523287f9b5c30957c
brandonricharda <brandonricharda@gmail.com>
2020-08-31 21:48:58 -0400

    Makes pawn class more readable.

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -11,26 +11,21 @@ class Pawn < Player
     end
 
     def potential_moves
-
         result = []
-
         #accounts for the fact that white pawns move "backwards"
         #because they sit at the opposite end of the board
         if @team == "black"
             result << top(@position)[0]
-            result << top(@position)[1]
+            result << top(@position)[1] unless @total_moves > 0
             result << top_left(@position)[-1]
             result << top_right(@position)[0]
         elsif @team == "white"
             result << bottom(@position)[-1]
-            result << bottom(@position)[-2]
+            result << bottom(@position)[-2] unless @total_moves > 0
             result << bottom_left(@position)[0]
             result << bottom_right(@position)[-1]
         end
-
-        #returns non-nil values
         result.select { |coordinates| coordinates }
-
     end
 
 end
