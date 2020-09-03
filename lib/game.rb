c4316ea4016e3494ab8b56c52bd115ac9348a106
brandonricharda <brandonricharda@gmail.com>
2020-09-02 21:44:21 -0400

    Adds team instance variables to game class.

--- lib/game.rb
+++ lib/game.rb
@@ -4,6 +4,8 @@ class Game
     attr_accessor :board
     def initialize
         @board = Board.new
+        @black_team = BlackTeam.new
+        @white_team = WhiteTeam.new
     end
 
     #in the initialize method we need a team instance variable
