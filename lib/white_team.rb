e21f07ff6fc11828953fd058f59b4ccfce0365c0
brandonricharda <brandonricharda@gmail.com>
2020-09-02 21:17:14 -0400

    Adds create_pieces functionality to white_team class.

--- lib/white_team.rb
+++ lib/white_team.rb
@@ -1,16 +1,31 @@
 Dir["*.rb"].each { |file| require_relative file }
 
-class BlackTeam
-    attr_accessor :starting_positions, :board_reference
+class WhiteTeam
+    attr_accessor :pieces
     def initialize
         @board_reference = Board.new.vertices.keys
-        @starting_positions = {
-            "rooks" => [[0, 7], [7, 7]],
-            "knights" => [[1, 7], [6, 7]],
-            "bishops" => [[2, 7], [5, 7]],
-            "queen" => [[3, 7]],
-            "king" => [[4, 7]],
-            "pawns" => @board_reference.select { |coordinates| coordinates[1] == 6 }
+        @pieces = create_pieces
+    end
+
+    def get_positions
+        result = {
+            Rook => [[0, 7], [7, 7]],
+            Knight => [[1, 7], [6, 7]],
+            Bishop => [[2, 7], [5, 7]],
+            Queen => [[3, 7]],
+            King => [[4, 7]],
+            Pawn => @board_reference.select { |coordinates| coordinates[1] == 6 }
         }
     end
+
+    def create_pieces
+        result = []
+        get_positions.each do |piece, positions_array|
+            positions_array.each do |coordinates|
+                result << piece.new(coordinates, "white")
+            end
+        end
+        result
+    end
+
 end
