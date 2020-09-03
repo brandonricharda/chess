6c6c3f1ecc1832a87037851dd0f3436fbe998acc
brandonricharda <brandonricharda@gmail.com>
2020-09-02 21:22:57 -0400

    Refactors black_team class positions module.

--- lib/black_team.rb
+++ lib/black_team.rb
@@ -1,16 +1,21 @@
 Dir["*.rb"].each { |file| require_relative file }
 
-class BlackTeam
+class BlackTeam > Team
     attr_accessor :starting_positions, :board_reference
     def initialize
         @board_reference = Board.new.vertices.keys
-        @starting_positions = {
-            "rooks" => [[0, 0], [7, 0]],
-            "knights" => [[1, 0], [6, 0]],
-            "bishops" => [[2, 0], [5, 0]],
-            "queen" => [[3, 0]],
-            "king" => [[4, 0]],
-            "pawns" => @board_reference.select { |coordinates| coordinates[1] == 1 }
+        @pieces = create_pieces
+    end
+
+    def get_positions
+        result = {
+            Rook => [[0, 0], [7, 0]],
+            Knight => [[1, 0], [6, 0]],
+            Bishop => [[2, 0], [5, 0]],
+            Queen => [[3, 0]],
+            King => [[4, 0]],
+            Pawn => @board_reference.select { |coordinates| coordinates[1] == 1 }
         }
     end
+
 end
