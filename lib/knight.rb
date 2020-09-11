69e701f4ad8c8e57e44f60e8f8e4e4b18dbabe34
brandonricharda <brandonricharda@gmail.com>
2020-09-10 23:54:19 -0400

    Adds upper bound for axis range to prevent pieces going off board.

--- lib/knight.rb
+++ lib/knight.rb
@@ -12,7 +12,8 @@ class Knight < Player
         x = @position[0]
         y = @position[1]
         single_square = 1
-        board_axis_edge = 0
+        min_board_edge = 0
+        max_board_edge = 7
         upward_left = [x - single_square, y + (single_square * 2)]
         upward_right = [x + single_square, y + (single_square * 2)]
         sideways_left_up = [x - (single_square * 2), y + single_square]
@@ -21,6 +22,9 @@ class Knight < Player
         downward_right = [x + single_square, y - (single_square * 2)]
         sideways_left_down = [x - (single_square * 2), y - single_square]
         sideways_right_down = [x + (single_square * 2), y - single_square]
-        [upward_left, upward_right, sideways_left_up, sideways_right_up, downward_left, downward_right, sideways_left_down, sideways_right_down].select { |coordinates| coordinates.all? { |axis| axis >= board_axis_edge } }
+        [upward_left, upward_right, sideways_left_up, sideways_right_up, downward_left, downward_right, sideways_left_down, sideways_right_down].select { |coordinates| coordinates.all? { |axis| (min_board_edge..max_board_edge).include?(axis) } }
     end
 end
+
+test = Knight.new([7, 7], "black")
+p test.potential_moves
