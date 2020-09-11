dd87ec2ad4a65d73c861f82fe99cb960f2a9f7e5
brandonricharda <brandonricharda@gmail.com>
2020-09-10 22:47:11 -0400

    Refactors potential_moves.

--- lib/knight.rb
+++ lib/knight.rb
@@ -9,25 +9,19 @@ class Knight < Player
     end
 
     def potential_moves
-        result = []
         x = @position[0]
         y = @position[1]
-        #upward L left
-        result << [x - 1, y + 2]
-        #upward L right
-        result << [x + 1, y + 2]
-        #sideways L left up
-        result << [x - 2, y + 1]
-        #sideways L right up
-        result << [x + 2, y + 1]
-        #downward L left
-        result << [x - 1, y - 2]
-        #downward L right
-        result << [x + 1, y - 2]
-        #sideways L left down
-        result << [x - 2, y - 1]
-        #sideways L right down
-        result << [x + 2, y - 1]
-        result.select { |coordinates| coordinates.all? { |axis| axis >= 0 } }
+        upward_left = [x - 1, y + 2]
+        upward_right = [x + 1, y + 2]
+        sideways_left_up = [x - 2, y + 1]
+        sideways_right_up = [x + 2, y + 1]
+        downward_left = [x - 1, y - 2]
+        downward_right = [x + 1, y - 2]
+        sideways_left_down = [x - 2, y - 1]
+        sideways_right_down = [x + 2, y - 1]
+        [upward_left, upward_right, sideways_left_up, sideways_right_up, downward_left, downward_right, sideways_left_down, sideways_right_down].select { |coordinates| coordinates.all? { |axis| axis >= 0 } }
     end
 end
+
+test = Knight.new([0, 0], "black")
+p test.potential_moves
