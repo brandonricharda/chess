5c334a9d29ec16484c678aeafb9a5f056f5b0abb
brandonricharda <brandonricharda@gmail.com>
2020-09-10 23:15:43 -0400

    Eliminates magic numbers.

--- lib/knight.rb
+++ lib/knight.rb
@@ -11,15 +11,17 @@ class Knight < Player
     def potential_moves
         x = @position[0]
         y = @position[1]
-        upward_left = [x - 1, y + 2]
-        upward_right = [x + 1, y + 2]
-        sideways_left_up = [x - 2, y + 1]
-        sideways_right_up = [x + 2, y + 1]
-        downward_left = [x - 1, y - 2]
-        downward_right = [x + 1, y - 2]
-        sideways_left_down = [x - 2, y - 1]
-        sideways_right_down = [x + 2, y - 1]
-        [upward_left, upward_right, sideways_left_up, sideways_right_up, downward_left, downward_right, sideways_left_down, sideways_right_down].select { |coordinates| coordinates.all? { |axis| axis >= 0 } }
+        single_square = 1
+        board_axis_edge = 0
+        upward_left = [x - single_square, y + (single_square * 2)]
+        upward_right = [x + single_square, y + (single_square * 2)]
+        sideways_left_up = [x - (single_square * 2), y + single_square]
+        sideways_right_up = [x + (single_square * 2), y + single_square]
+        downward_left = [x - single_square, y - (single_square * 2)]
+        downward_right = [x + single_square, y - (single_square * 2)]
+        sideways_left_down = [x - (single_square * 2), y - single_square]
+        sideways_right_down = [x + (single_square * 2), y - single_square]
+        [upward_left, upward_right, sideways_left_up, sideways_right_up, downward_left, downward_right, sideways_left_down, sideways_right_down].select { |coordinates| coordinates.all? { |axis| axis >= board_axis_edge } }
     end
 end
 
