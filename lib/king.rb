90583df1e029865c933462094b87c77232fc7a15
brandonricharda <brandonricharda@gmail.com>
2020-09-14 22:34:57 -0400

    Refactors potential_moves.

--- lib/king.rb
+++ lib/king.rb
@@ -6,28 +6,14 @@ class King < Player
         super(position, color)
         options = {"white" => "\u2654", "black" => "\u265A"}
         @symbol = options[color]
+        @legal_movements = {
+            :upright => [0, 1],
+            :sideways => [1, 0],
+            :diagonal => [1, 1]
+        }
     end
 
     def potential_moves
-        result = []
-        x = @position[0]
-        y = @position[1]
-        #one forward
-        result << [x, y + 1]
-        #one backward
-        result << [x, y - 1]
-        #one right
-        result << [x + 1, y]
-        #one left
-        result << [x - 1, y]
-        #diagonal up right
-        result << [x + 1, y + 1]
-        #diagonal up left
-        result << [x - 1, y + 1]
-        #diagonal down right
-        result << [x + 1, y - 1]
-        #diagonal down left
-        result << [x - 1, y - 1]
-        result.select { |coordinates| coordinates.all? { |axis| axis >= 0 } }
+        parse_potential_moves(@legal_movements)
     end
 end
