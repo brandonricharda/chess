2f0705f4f707c4cc1c148df188841bdc724afd39
brandonricharda <brandonricharda@gmail.com>
2020-09-15 21:35:46 -0400

    Adds legal_movements to Bishop.

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -3,15 +3,19 @@ require_relative "player.rb"
 include All_Directions
 
 class Bishop < Player
-    attr_accessor :symbol
+    attr_accessor :symbol, :legal_movements
     def initialize(position, color)
         super(position, color)
         options = {"white" => "\u2657", "black" => "\u265D"}
         @symbol = options[color]
+        @legal_movements = {
+            :first_layer   => [1, 1],
+            :second_layer  => [2, 2],
+            :third_layer   => [3, 3],
+            :fourth_layer  => [4, 4],
+            :fifth_layer   => [5, 5],
+            :sixth_layer   => [6, 6],
+            :seventh_layer => [7, 7]
+        }
     end
-
-    def potential_moves
-        top_left(@position) + top_right(@position) + bottom_right(@position) + bottom_left(@position)
-    end
-
 end
