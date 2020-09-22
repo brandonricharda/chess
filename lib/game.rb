a5c96270cf7f243d623fba18a61771efdd4faa22
brandonricharda <brandonricharda@gmail.com>
2020-09-21 22:39:52 -0400

    Adds select_move method.

--- lib/game.rb
+++ lib/game.rb
@@ -13,7 +13,7 @@ class Game
         return false unless piece.potential_moves(piece.legal_movements).include?(move)
         opponent = ["black", "white"].select { |team| team != piece.team }
         diagonal = (move[0] - piece.position[0]).abs == 1
-        if piece.pece == "Pawn"
+        if piece.piece == "Pawn"
             return @board.occupancies[move] == opponent if diagonal
             @board.occupancies[move] == nil
         else
@@ -25,5 +25,19 @@ class Game
     def determine_capture(piece, move)
         opponent = ["black", "white"].select { |team| team != piece.team }
         @board.occupancies[move] == opponent
+    end
+
+    def select_move(team)
+        team.selector
+        piece = team.selected_piece
+        legal_moves = piece.potential_moves(piece.legal_movements).select { |move| validate_move(piece, move) }
+        p "#{team.color.upcase} #{piece.piece.upcase}, please enter the move you would like to make."
+        move = JSON.parse(gets.chomp)
+        until validate_move(piece, move)
+            p "Please ensure you have entered a valid move. It should be an array with [x, y] coordinates."
+            p "This piece can move to: #{legal_moves}."
+            move = JSON.parse(gets.chomp)
+        end
+        move
     end
 end
